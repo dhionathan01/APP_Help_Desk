c42eb1d6861c23d9b295cfaa98aa61aaecfa57df
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-04-06 16:54:28 -0300

    8 - Criando Lógica para Abertura de chamados

---
+++ registra_chamado.php
@@ -0,0 +1,27 @@
+<?
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
+    //Modificando o caracter que é usado para separação dos dados, com intuito de garantir a integridade da informação
+    $titulo = str_replace('#', ' - ', $_POST['titulo']);
+    $categoria = str_replace('#', ' - ', $_POST['categoria']);
+    $descricao = str_replace('#', ' - ', $_POST['descricao']); 
+
+
+
+    //Abrindo um arquivo:
+    $arquivo = fopen('arquivo.hd', 'a'); // Caso o arquivo não existir ele será criado
+
+
+    // Transformando o Array em texto:
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL ;
+
+    // Escrevendo no arquivo
+    fwrite($arquivo, $texto);
+
+    // Fechando o arquivo
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php')
+?>
