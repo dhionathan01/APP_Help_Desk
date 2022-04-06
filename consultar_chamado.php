8df8728bcde5074b48e70a9b4811b05440d04adc
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-04-06 17:30:37 -0300

    9 - Permitindo a  visualização dos chamados no front-end

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -18,10 +18,6 @@
   }
   // Fechando arquivo
   fclose($arquivo);
-
-  echo '<pre>';
-  print_r($chamados);
-  echo '<pre>';
 ?>
 
 <html>
@@ -64,24 +60,27 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <!-- Para cada linha criar uma string chamado -->
+              <? foreach($chamados as $chamado){ ?>
+                <?php
+                  /* pegar o chamado e por meio do separador # criar um array com cada conteudo */
+                  $chamado_dados = explode('#', $chamado);
 
+                  if(count($chamado_dados) < 3 ){ // Se estiver faltando alguma informação ignore o restante do bloco
+                    continue;
+                  }
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <!-- Atribuindo o valor contido em cada índice do array no lugar correto -->
+                    <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                    <p class="card-text"><?= $chamado_dados[2] ?></p> 
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
